385e089c2b998c119778f8ce7c6d390985bb0223
AlexAxenti <alex.axenti@gmail.com>
2020-12-26 19:28:59 -0500

    Setting up gui file

--- src/LoginGUI.java
+++ src/LoginGUI.java
@@ -1,4 +1,22 @@
-public class LoginGUI {
+package application;
 
-    //GUI for Login page
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+public class LoginGUI extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+	
 }
