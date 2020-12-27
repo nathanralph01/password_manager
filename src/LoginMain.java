f70634a018dc74030894be9c614fe11cfd8ad690
nathanralph01 <nathanralph01@gmail.com>
2020-12-26 19:57:20 -0500

    Added setFile and getFile methods

--- src/LoginMain.java
+++ src/LoginMain.java
@@ -1,4 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class LoginMain {
 
-    // Main class that handles Login/Create account
+    private String key;
+
+    public LoginMain(String key){
+        this.key = key;
+    }
+
+    public void getFile(){
+        // Finds text file associated with key
+        // Raise error/ask to create file if file does not exist
+        try {
+            BufferedReader in_file = new BufferedReader(new FileReader(this.key));
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e);
+        }
+
+    }
+
+    public void setFile(){
+        // Creates new text file and its associated key
+        // Checks if the key already exists
+    }
+
 }
