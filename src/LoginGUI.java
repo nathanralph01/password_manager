88ecaafdc392e2cd91c8441d577ce161cefc3f72
AlexAxenti <alex.axenti@gmail.com>
2020-12-26 20:32:35 -0500

    Basic layout work

--- src/LoginGUI.java
+++ src/LoginGUI.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -17,6 +19,8 @@ import javafx.scene.text.Text;
 public class LoginGUI extends Application {
 	
 	Scene login;
+	Scene mainScene;
+	Stage window;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -30,7 +34,7 @@ public class LoginGUI extends Application {
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
 		
-		Text formTitle = new Text ("Enter a password below!");
+		Text formTitle = new Text ("Enter a username below");
 		formTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 		grid.add(formTitle, 0, 0, 2, 1);
 		
@@ -38,22 +42,66 @@ public class LoginGUI extends Application {
 		grid.add(passwordTF, 0, 1, 2, 1);
 		
 		Button loginButton = new Button("Login");
+		loginButton.setOnAction(e -> {
+			window.setScene(mainScene);
+		});
 		grid.add(loginButton, 0, 2);
 		
 		Button registerButton = new Button("Register");
 		grid.add(registerButton, 1, 2);
 		
-		login = new Scene(grid,400,300);
+		login = new Scene(grid,500,300);
+		
+	}
+	
+	public void mainScene() {
+		
+		BorderPane border = new BorderPane();
+		HBox header = new HBox(50);
+		header.setAlignment(Pos.CENTER);
+		
+		Button add = new Button("Add Entry");
+		Button logout = new Button("Logout");
+		Button edit = new Button ("Edit");
+		
+		header.getChildren().addAll(add,logout,edit);
+		border.setTop(header);
+		
+	    
+		
+		ScrollPane content = new ScrollPane();
+		VBox vbox = new VBox(15);
+		
+		for (int i = 0; i < 3; i++) {
+			HBox hbox = new HBox(25);
+			
+			Label label1 = new Label("Test1");
+			Label label2 = new Label("Test2");
+			
+			hbox.getChildren().addAll(label1, label2);
+			vbox.getChildren().add(hbox);
+			
+			
+		}
+		
+		content.setContent(vbox);
+		
+		border.setCenter(content);
+		
+		mainScene = new Scene(border, 500, 300);
 		
 	}
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-		loginScene();
+		window = primaryStage;
 		
-		primaryStage.setScene(login);
-		primaryStage.show();
+		loginScene();
+		mainScene();
+		
+		window.setScene(login);
+		window.show();
 		
 	}
 	
